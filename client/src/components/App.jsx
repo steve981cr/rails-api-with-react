315a26b652052d50af1fc64c1a70867e6baa751d
Steve Carey <steve981cr@gmail.com>
2019-07-01 14:28:53 -0700

    minor update

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
 import '../stylesheets/App.css';
 import Home from './Home';
 import ArticleList from './ArticleList';
@@ -11,14 +10,7 @@ import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>      
+      <div className="App">  
         <Router>
           <div className="container">
             <Navigation />
